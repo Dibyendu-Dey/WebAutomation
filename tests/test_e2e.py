fad2dd2fb4dc59988399cd9c143d65be04d9aa42
Dibyendu <deybittu715@gmail.com>
2024-06-14 20:18:48 +0530

    Base class created to hold all the common utilities

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,16 +1,18 @@
 import pytest
+from Utilities.BaseClass import BaseClass
 
 
-@pytest.mark.usefixtures("setup")
-class TestE2E:
+# @pytest.mark.use-fixtures("setup")  # This statement is no longer needed as parent BaseClass has the idea of setup
+class TestE2E(BaseClass):  # Now we are inheriting all the properties and method of the parent Base class
     """
     TestE2E is a Pytest clas to wrap all the Pytest test methods
     """
 
-    def test_radio_button_example(self):
+    def test_radio_button(self):
         """Pytest test method to handle radio button in page"""
-        pass
+        self.get_logger().info("TC: test_radio_button passed")
+        print(self.data["TestData"]["RadioButton"])
 
     def test_suggestive_dropdown(self):
         """Pytest test method to handle suggestive dropdown in page"""
-        pass
+        self.get_logger().info("TC: test_suggestive dropdown passed")
